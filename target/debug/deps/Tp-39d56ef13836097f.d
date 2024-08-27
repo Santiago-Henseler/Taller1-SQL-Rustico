97cf4543e3f06409431b578fdc73ab8f128a9d3e
Santy <shenseler@fi.uba.ar>
2024-08-27 14:30:59 -0300

    initial commit

---
+++ target/debug/deps/Tp-39d56ef13836097f.d
@@ -0,0 +1,5 @@
+/media/santy/2tb/facultad/taller1/Taller1-SQL-Rustico/target/debug/deps/libTp-39d56ef13836097f.rmeta: src/main.rs
+
+/media/santy/2tb/facultad/taller1/Taller1-SQL-Rustico/target/debug/deps/Tp-39d56ef13836097f.d: src/main.rs
+
+src/main.rs:
